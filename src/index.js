6faa2368ffb14e1cb739ca76b7612e5d101c2a71
Nikema <3941856+prophen@users.noreply.github.com>
2022-09-06 17:12:45 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+import { DonateApp } from "./DonateApp";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <DonateApp />
+  </StrictMode>,
+  rootElement
+);
